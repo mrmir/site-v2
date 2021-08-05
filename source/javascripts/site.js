6797f70e2dac3b72602485032be7c0803f6dccc8
mrmir <mirdotbhatia@gmail.com>
2021-08-05 20:50:37 +0530

    Update CSS and add JS

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,16 @@
-// This is where it all goes :)
+window.addEventListener('DOMContentLoaded', function() {
+  var mir = document.getElementsByClassName('mir')[0];
+  var body = document.body;
+
+  mir.addEventListener('mouseenter', function () {
+    body.classList.add('focused');
+  }, false);
+
+  mir.addEventListener('mouseleave', function() {
+    body.classList.remove('focused');
+  }, false);
+
+  mir.addEventListener('click', function () {
+    body.classList.remove('focused');
+  }, false);
+});
